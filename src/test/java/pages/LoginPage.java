4f4ce63b3c96f49a6bd7620c8682850e48aeef95
Vasilisa222024 <155727090+Vasilisa222024@users.noreply.github.com>
2024-02-24 19:10:49 +0200

    commit new

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+
+    public class LoginPage extends BasePage{
+        @FindBy(xpath = "//input[@name='email']")
+        WebElement emailField;
+
+        @FindBy(xpath = "//button[@name='registration']")
+        WebElement registrationButton;
+
+        public LoginPage(WebDriver driver){
+            setDriver(driver);
+            PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20), this);
+        }
+    }
+
